43dafbffef14e9003edf855de7301685358b044c
Dams168 <adamrohman2003@gmail.com>
2024-11-08 13:07:50 +0700

    add: updateimage

--- controllers/mediaController.js
+++ controllers/mediaController.js
@@ -43,6 +43,75 @@ class mediaController {
             });
         }
     }
+
+    static async updateImage(req, res) {
+        try {
+            const { id } = req.params;
+            const { title, description } = req.body;
+            const file = req.file;
+
+            if (!title || !description) {
+                return res.status(400).json({
+                    status: 'error',
+                    message: 'title and description harus diisi'
+                });
+            }
+            if (!file) {
+                return res.status(400).json({
+                    status: 'error',
+                    message: 'file harus diisi'
+                });
+            }
+            // console.log(file);
+
+            const image = await prisma.image.findUnique({
+                where: {
+                    id: Number(id)
+                }
+            });
+
+            if (!image) {
+                return res.status(404).json({
+                    status: 'error',
+                    message: 'image not found'
+                });
+            }
+
+            const stringFile = file.buffer.toString('base64');
+
+            const deleteImage = await imagekit.deleteFile(image.imageFileId);
+            // console.log(deleteImage);
+
+            const updateImage = await imagekit.upload({
+                fileName: file.originalname,
+                file: stringFile
+            });
+
+            const result = await prisma.image.update({
+                where: {
+                    id: Number(id)
+                },
+                data: {
+                    title,
+                    description,
+                    imageUrl: updateImage.url,
+                    imageFileId: updateImage.fileId
+                }
+            });
+
+            res.status(200).json({
+                status: 'success',
+                message: 'update image success',
+                data: result
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: 'error',
+                message: error.message
+            });
+        }
+
+    }
 }
 
 module.exports = mediaController;
