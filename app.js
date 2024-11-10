21b762f2f34f938d38db0775145ffa940b359b47
Dams168 <adamrohman2003@gmail.com>
2024-11-10 20:26:01 +0700

    update: app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const express = require('express');
 const mediaRoutes = require('./routes/mediaRoutes');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', mediaRoutes);
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World');
-// });
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
